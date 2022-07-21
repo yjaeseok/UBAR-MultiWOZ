d5da02f39bc5b4f034586e5ffa7c1c468a851c48
Jaeseok Yoon <jake.yoon@kaist.ac.kr>
2022-07-21 15:55:06 +0900

    Remove URURU feature and add wandb for logging

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ import logging
 import json
 import tqdm
 import numpy as np
-from torch.utils.tensorboard import SummaryWriter
+import wandb
 from tqdm import tqdm
 
 from config import global_config as cfg 
@@ -45,10 +45,7 @@ class Modal(object):
 
         #
         self.evaluator = MultiWozEvaluator(self.reader)
-        if cfg.save_log and cfg.mode == 'train':
-            self.tb_writer = SummaryWriter(log_dir='./log')
-        else:
-            self.tb_writer = None
+        wandb.init(project="ubar", entity="kaist-dstc11-track3", config=cfg)
 
     def get_optimizers(self):
         """
@@ -122,130 +119,6 @@ class Modal(object):
         loss /= num_targets
         return loss
 
-    def train_URURU(self):
-        """
-        URURU
-        """
-        all_batches = self.reader.get_batches('train')
-        # compute num_training_steps in get_batches()
-        optimizer, scheduler = self.get_optimizers()
-
-        # log info
-        set_stats = self.reader.set_stats['train']
-        logging.info("***** Running training *****")
-        logging.info("  Num Training steps(one turn in a batch of dialogs) per epoch = %d",
-                     set_stats['num_training_steps_per_epoch'])
-        logging.info("  Num Turns = %d", set_stats['num_turns'])
-        logging.info("  Num Dialogs = %d", set_stats['num_dials'])
-        logging.info("  Num Epochs = %d", cfg.epoch_num)
-        logging.info("  Batch size  = %d", cfg.batch_size)
-        logging.info("  Gradient Accumulation steps = %d",
-                     cfg.gradient_accumulation_steps)
-        logging.info("  Total optimization steps = %d",
-                     set_stats['num_training_steps_per_epoch']*cfg.epoch_num // cfg.gradient_accumulation_steps)
-
-        # tb writer
-        if self.tb_writer is not None:
-            self.tb_writer.add_text('cfg', json.dumps(cfg.__dict__, indent=2))
-            # self.tb_writer.add_hparams(self.args.to_sanitized_dict(), metric_dict={})
-
-        log_inputs = 2
-        global_step = 0
-        sw = time.time()
-
-        for epoch in range(cfg.epoch_num):
-            epoch_step = 0
-            tr_loss = 0.0
-            logging_loss = 0.0
-            btm = time.time()
-            oom_time = 0
-            self.model.zero_grad()
-
-            data_iterator = self.reader.get_data_iterator(
-                all_batches)
-
-            for batch_idx, dial_batch in enumerate(data_iterator):
-                pv_batch = None
-                for turn_num, turn_batch in enumerate(dial_batch):
-                    first_turn = (turn_num == 0)
-                    inputs = self.reader.convert_batch_turn(turn_batch, pv_batch, first_turn)
-                    pv_batch = inputs['labels']
-                    try:  # avoid OOM
-                        self.model.train()
-                        if log_inputs > 0:  # log inputs for the very first two turns
-                            self.log_first_inputs(inputs)
-                            log_inputs -= 1
-
-                        # to tensor
-                        inputs = self.add_torch_input(inputs)
-                        # loss
-                        outputs = self.model(inputs['contexts_tensor'])
-                        # outputs = self.model(inputs['contexts_tensor']) # debugging with GPT2Model
-                        loss = self.calculate_loss_and_accuracy(
-                            outputs, labels=inputs['contexts_tensor'])
-                        loss.backward()
-                        tr_loss += loss.item()
-                        torch.nn.utils.clip_grad_norm_(
-                            self.model.parameters(), 5.0)
-                        epoch_step += 1
-
-                        # step, wrt gradient_accumulation_steps, clip grad norm
-                        if (epoch_step+1) % cfg.gradient_accumulation_steps == 0 or(
-                            # end of an epoch
-                            (epoch_step + \
-                            1) == set_stats['num_training_steps_per_epoch']
-                        ):
-                            optimizer.step()
-                            scheduler.step()
-                            optimizer.zero_grad()
-                            # global_step: actual step the optimizer took
-                            global_step += 1
-
-                            logs = {}  # for tb writer
-                            # logging: loss, lr... after certain amount of steps
-                            if cfg.report_interval > 0 and global_step % cfg.report_interval == 0:
-                                loss_scalar = (tr_loss - logging_loss) / \
-                                    cfg.report_interval
-                                logging_loss = tr_loss
-                                logs['loss'] = loss_scalar
-                                logging.info(
-                                    'Global step: {}, epoch step: {}, interval loss: {:.4f}'.format(
-                                        global_step, epoch_step, loss_scalar
-                                    ))
-                                # validate
-                                # add to tensorboard...
-                                if cfg.evaluate_during_training and loss_scalar < 10:
-                                    results = self.validate()
-                                    for k, v in results.items():
-                                        eval_key = "eval_{}".format(k)
-                                        logs[eval_key] = v
-
-                                if self.tb_writer:
-                                    for k, v in logs.items():
-                                        self.tb_writer.add_scalar(
-                                            k, v, global_step)
-                                # save model... 
-
-                    except RuntimeError as exception:
-                        if "out of memory" in str(exception):
-                            max_length = max(inputs['lengths'])
-                            oom_time += 1
-                            logging.info("WARNING: ran out of memory,times: {}, batch size: {}, max_len: {}".format(
-                                oom_time, cfg.batch_size, max_length))
-                            if hasattr(torch.cuda, 'empty_cache'):
-                                torch.cuda.empty_cache()
-                        else:
-                            logging.info(str(exception))
-                            raise exception
-            logging.info('Train epoch time: {:.2f} min, epoch loss: {:.4f}'.format(
-                (time.time()-btm)/60, tr_loss))
-            # save model after every epoch
-            # if epoch > 30 and tr_loss/epoch_step < 0.6:
-            self.save_model(epoch, tr_loss/epoch_step)
-
-
-
-
     def train(self):
         """
         UBARU
@@ -267,11 +140,6 @@ class Modal(object):
                      cfg.gradient_accumulation_steps)
         logging.info("  Total optimization steps = %d",
                      set_stats['num_dials']*cfg.epoch_num // (cfg.gradient_accumulation_steps*cfg.batch_size))
-
-        # tb writer
-        if self.tb_writer is not None:
-            self.tb_writer.add_text('cfg', json.dumps(cfg.__dict__, indent=2))
-            # self.tb_writer.add_hparams(self.args.to_sanitized_dict(), metric_dict={})
 
         log_inputs = 2
         global_step = 0
@@ -340,10 +208,7 @@ class Modal(object):
                                     eval_key = "eval_{}".format(k)
                                     logs[eval_key] = v
 
-                            if self.tb_writer:
-                                for k, v in logs.items():
-                                    self.tb_writer.add_scalar(
-                                        k, v, global_step)
+                            wandb.log(logs)
                             # save model... 
 
                 except RuntimeError as exception:
@@ -375,125 +240,6 @@ class Modal(object):
         self.tokenizer.save_pretrained(save_path)
         # save cfg
 
-    def validate_URURU(self, data='dev', do_test=False):
-        # predict one dialog/ one turn at a time
-        self.model.eval()
-
-        # all_batches = self.reader.get_batches('dev')
-        # data_iterator = self.reader.get_data_iterator(all_batches)
-        eval_data = self.reader.get_eval_data(data)
-
-        set_stats = self.reader.set_stats[data]
-        logging.info("***** Running Evaluation *****")
-        logging.info("  Num Turns = %d", set_stats['num_turns'])
-        # logging.info("  Num Dialogs = %d", set_stats['num_dials'])
-
-        # valid_losses = []
-        btm = time.time()
-        result_collection = {}
-        with torch.no_grad():
-            eval_pbar = eval_data
-            for dial_idx, dialog in enumerate(eval_pbar):
-
-                pv_turn = {}
-                for turn_idx, turn in enumerate(dialog):
-                    first_turn = (turn_idx == 0)
-                    inputs = self.reader.convert_turn_eval_URURU(
-                        turn, pv_turn, first_turn)
-                    inputs = self.add_torch_input_eval(inputs)
-
-                    # fail to generate new tokens, if max_length not set
-                    context_length = len(inputs['context'])
-                    if cfg.use_true_curr_bspn: # generate act, response
-                        max_len=60
-                        if not cfg.use_true_curr_aspn:
-                            max_len = 80
-
-                        outputs = self.model.generate(input_ids=inputs['context_tensor'],
-                                                    max_length=context_length+max_len, temperature=0.7, # top_p=0.9, num_beams=4,
-                                                    pad_token_id=self.tokenizer.eos_token_id, eos_token_id=self.tokenizer.encode(['<eos_r>'])[0])
-                                                    #   no_repeat_ngram_size=4
-                        # turn['generated'] = self.tokenizer.decode(outputs[0])
-
-                        # resp_gen, need to trim previous context
-                        generated = outputs[0].cpu().numpy().tolist()
-                        generated = generated[context_length-1:]
-
-                        try:
-                            decoded = self.decode_generated_act_resp(generated)
-                        except ValueError as exception:
-                            logging.info(str(exception))
-                            logging.info(self.tokenizer.decode(generated))
-                            decoded = {'resp': [], 'bspn': [], 'aspn': []}
-
-                    else: # predict bspn, access db, then generate act and resp
-                        outputs = self.model.generate(input_ids=inputs['context_tensor'],
-                                                    max_length=context_length+60, temperature=0.7, # top_p=0.9, num_beams=4,
-                                                    pad_token_id=self.tokenizer.eos_token_id, eos_token_id=self.tokenizer.encode(['<eos_b>'])[0])
-                        generated_bs = outputs[0].cpu().numpy().tolist()
-                        # generated_bs = generated_bs[context_length-1:]
-                        bspn_gen = self.decode_generated_bspn(generated_bs[context_length-1:])
-                        # check DB result
-                        if cfg.use_true_db_pointer:
-                            # db_result = self.reader.bspan_to_DBpointer(self.tokenizer.decode(turn['bspn']), turn['turn_domain'])
-                            db = turn['db']
-                        else:
-                            db_result = self.reader.bspan_to_DBpointer(self.tokenizer.decode(bspn_gen), turn['turn_domain'])
-                            db = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize('<sos_db> '+ db_result + ' <eos_db>')) + self.tokenizer.encode(['<sos_a>'])
-                        inputs['context_tensor_db'] = torch.tensor([inputs['context'][:-1] + bspn_gen + db]).to(self.device)
-                        context_length = len(inputs['context_tensor_db'][0])
-                        outputs_db = self.model.generate(input_ids=inputs['context_tensor_db'],
-                                                    max_length=context_length+80, temperature=0.7, # top_p=0.9, num_beams=4,
-                                                    pad_token_id=self.tokenizer.eos_token_id, eos_token_id=self.tokenizer.encode(['<eos_r>'])[0])
-                        generated_ar = outputs_db[0].cpu().numpy().tolist()
-                        generated_ar = generated_ar[context_length-1:]
-                        try:
-                            decoded = self.decode_generated_act_resp(generated_ar)
-                            decoded['bspn'] = bspn_gen
-                        except ValueError as exception:
-                            logging.info(str(exception))
-                            logging.info(self.tokenizer.decode(generated_ar))
-                            decoded = {'resp': [], 'bspn': [], 'aspn': []}
-                    
-                    turn['resp_gen'] = decoded['resp']
-                    turn['bspn_gen'] = turn['bspn'] if cfg.use_true_curr_bspn else decoded['bspn']
-                    turn['aspn_gen'] = turn['aspn'] if cfg.use_true_curr_aspn else decoded['aspn']
-                    turn['dspn_gen'] = turn['dspn']
-
-                    # check DB results
-                    # db_result = self.reader.bspan_to_DBpointer(self.tokenizer.decode(turn['bspn']), turn['turn_domain'])
-                    # if db_result[0] == 1: # no match
-                    #     print('gt:', self.tokenizer.decode(turn['aspn']), '     |gen:', self.tokenizer.decode(decoded['aspn']))
-                    #     print('gen_resp: ', self.tokenizer.decode(decoded['resp']))
-                    #     print('gt_resp: ', self.tokenizer.decode(turn['resp']), '\n')
-
-                    pv_turn['labels'] = inputs['labels'] # all true previous context
-                    pv_turn['resp'] = turn['resp'] if cfg.use_true_prev_resp else decoded['resp']
-                    # pv_turn['bspn'] = turn['bspn'] if cfg.use_true_prev_bspn else decoded['bspn']
-                    # pv_turn['db'] = db
-                    # pv_turn['aspn'] = turn['aspn'] if cfg.use_true_prev_aspn else decoded['aspn']
-                    # pv_turn = inputs['labels']
-                    
-                result_collection.update(
-                    self.reader.inverse_transpose_turn(dialog))
-
-        logging.info("inference time: {:.2f} min".format((time.time()-btm)/60))
-        # score
-        btm = time.time()
-        results, _ = self.reader.wrap_result_lm(result_collection)
-        bleu, success, match = self.evaluator.validation_metric(results)
-        logging.info("Scoring time: {:.2f} min".format((time.time()-btm)/60))
-        score = 0.5 * (success + match) + bleu
-        valid_loss = 130 - score
-        logging.info('validation [CTR] match: %2.2f  success: %2.2f  bleu: %2.2f    score: %.2f' % (
-            match, success, bleu, score))
-        eval_results = {}
-        eval_results['bleu'] = bleu
-        eval_results['success'] = success
-        eval_results['match'] = match
-
-        return eval_results
-
     def validate(self, data='dev', do_test=False):
         # predict one dialog/ one turn at a time
         self.model.eval()
@@ -512,7 +258,9 @@ class Modal(object):
         result_collection = {}
         with torch.no_grad():
             for dial_idx, dialog in enumerate(eval_data):
-
+                if dialog[0]['dial_id'] == 'mul1762':
+                    print('Ignore skip dialog')
+                    continue
                 pv_turn = {}
                 for turn_idx, turn in enumerate(dialog):
                     first_turn = (turn_idx == 0)
@@ -560,9 +308,13 @@ class Modal(object):
                             db = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize('<sos_db> '+ db_result + ' <eos_db>')) + self.tokenizer.encode(['<sos_a>'])
                         inputs['context_tensor_db'] = torch.tensor([inputs['context'][:-1] + bspn_gen + db]).to(self.device)
                         context_length = len(inputs['context_tensor_db'][0])
-                        outputs_db = self.model.generate(input_ids=inputs['context_tensor_db'],
+                        try:
+                            outputs_db = self.model.generate(input_ids=inputs['context_tensor_db'],
                                                     max_length=context_length+80, temperature=0.7, # top_p=0.9, num_beams=4,
                                                     pad_token_id=self.tokenizer.eos_token_id, eos_token_id=self.tokenizer.encode(['<eos_r>'])[0])
+                        except RuntimeError:
+                            print(dialog[0]['dial_id'] + '_' + str(turn_idx))
+                            continue
                         generated_ar = outputs_db[0].cpu().numpy().tolist()
                         generated_ar = generated_ar[context_length-1:]
                         try:
@@ -609,7 +361,7 @@ class Modal(object):
         eval_results['success'] = success
         eval_results['match'] = match
         eval_results['score'] = score
-        eval_results['result'] = 'validation [CTR] match: %2.2f  success: %2.2f  bleu: %2.2f    score: %.2f' % (match, success, bleu, score)
+        # eval_results['result'] = 'validation [CTR] match: %2.2f  success: %2.2f  bleu: %2.2f    score: %.2f' % (match, success, bleu, score)
 
         
         model_setting, epoch_setting = cfg.eval_load_path.split('/')[1], cfg.eval_load_path.split('/')[2]
@@ -628,6 +380,7 @@ class Modal(object):
             eval_to_json[epoch_setting] = eval_results
             json.dump(eval_to_json, open(log_file_name, 'w'), indent=2)
         logging.info('update eval results to {}'.format(log_file_name))
+        wandb.log(eval_results)
         return eval_results
 
     def decode_generated_act_resp(self, generated):
@@ -758,8 +511,6 @@ def main():
             pass
         if cfg.context_scheme == 'UBARU':
             m.train()
-        elif cfg.context_scheme == 'URURU':
-            m.train_URURU()
         else:
             logging.info('Invalid context Scheme. must be UBARU or URURU')
             exit()
@@ -774,9 +525,6 @@ def main():
         if cfg.context_scheme == 'UBARU':
             m.validate()
             m.validate('test')
-        elif cfg.context_scheme == 'URURU':
-            m.validate_URURU()
-            m.validate_URURU('test')
 
         # logging.info('Running eavl on test')
         # m.validate('test')
